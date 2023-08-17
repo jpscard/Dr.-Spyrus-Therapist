c345e4367697df5dea35ba0622ae4b5ab4b48811
João Paulo Cardoso <78612298+jpscard@users.noreply.github.com>
2023-08-16 23:03:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ Se o paciente disser 'adeus' ou qualquer palavra que indique que ele/ela está e
 
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
-           string_dialogue += "User: " + dict_message["content"] + "\\n\\n"
+               string_dialogue += "User: " + dict_message["content"] + "\\n\\n"
         else:
-        string_dialogue += "Assistant: " + dict_message["content"] + "\\n\\n"
-        prompt_input =  '"""'+prompt_input+'"""'
-        output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ", "temperature":0.2, "top_p":0.8, "max_length":1024, "repetition_penalty":1})
+            string_dialogue += "Assistant: " + dict_message["content"] + "\\n\\n"
+            prompt_input =  '"""'+prompt_input+'"""'
+            output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ", "temperature":0.2, "top_p":0.8, "max_length":1024, "repetition_penalty":1})
     return output
 
 # User-provided prompt
